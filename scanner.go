4c775a50b88c9f783b0a92c1ac242f95371e96c7
cocatrip <cocainetrip@protonmail.com>
2021-10-25 13:57:25 +0700

    sync

--- scanner.go
+++ scanner.go
@@ -43,10 +43,10 @@ func init() {
 }
 
 func CallClear() {
-	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
-	if ok {                          //if we defined a clear func for that platform:
-		value() //we execute it
-	} else { //unsupported platform
+	value, ok := clear[runtime.GOOS]
+	if ok {
+		value()
+	} else {
 		panic("Your platform is unsupported! I can't clear terminal screen :(")
 	}
 }
